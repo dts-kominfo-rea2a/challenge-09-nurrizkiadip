d260c939f68f0ee31ee4eb0c22e01b7f99449756
nurrizkiadip <nrap.rizki10@gmail.com>
2022-06-26 16:29:57 +0700

    Finish challenge react intro

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,34 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
+import './Contact.css'
+
 import React from 'react';
 
-const Contact = () => {
-    return (
-        <></>
-    )
+const Contact = ({ contacts }) => {
+  return (
+      <>
+        <div className="contact-container">
+          {
+            contacts.map((contact) => {
+              return (
+                  <>
+                    <div className="contact-item">
+                      <div className="contact-item__image">
+                        <img src={contact.photo} alt={contact.name}/>
+                      </div>
+                      <div className="contact-item__info">
+                        <h3>{contact.name}</h3>
+                        <p>{contact.phone}</p>
+                        <p>{contact.email}</p>
+                      </div>
+                    </div>
+                  </>
+              )
+            })
+          }
+        </div>
+      </>
+  )
 }
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
+import './Header.css'
+
 import React from 'react';
 
 const Header = () => {
-    return (
-        <></>
-    )
+  return (
+      <>
+        <h1>Call a Friend</h1>
+        <div>your friendly contact app</div>
+      </>
+  )
 }
 
 export default Header;
